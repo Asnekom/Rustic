9f86f690bfed60abbd27e2176cbe4a001ef9f944
North <103772977+Asnekom@users.noreply.github.com>
2023-06-17 13:24:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -245,6 +245,10 @@ addCart.addEventListener('click', function() {
     }    
 })    
 
+function Refresh() {
+    window.parent.location = window.parent.location.href;
+}
+
 receiptBtn.addEventListener(' click', function() {
     location.reload();
 })
